94a7830c1f8aea8f765a30d6c8a02ce2f4ca87fe
Achiel van der Mandele <git@elsuxor.net>
2012-03-13 16:19:49 +0200

    get implemented, deletes key after returning, return url when creating

--- src/index.py
+++ src/index.py
@@ -3,16 +3,32 @@ app = Flask(__name__)
 import redis
 import random, string
 
+url = "http://localhost:5000"
 r = redis.Redis()
 r.set('foo', 'bar')
 
 random_char = lambda : random.choice(string.ascii_uppercase + string.digits)
+random_string = lambda x : ''.join(random_char() for x in range(x))
 @app.route("/create/", methods=['POST',])
 def create():
+    key = random_string(50)
     secret = request.form['secret']
     if not secret: 
-        secret = ''.join(random_char() for x in range(20))
-    return "hello world: %s" % secret
+        secret = random_string(20)
+    r.set(key, secret)
+    return "your secret is stored at %s/get/%s" % (url, key)
+
+@app.route("/get/<key>")
+def retrieve(key=None):
+    print key
+    if not key or not r.get(key):
+        return "key not found"
+    
+    val = r.get(key)
+    r.delete(key)
+    return "the following secret was retrieved: <br/> %s <br/> \
+            Please note that you will only be able to retrieve this secret once" \
+            % val
 
 if __name__ == "__main__":
     app.run()
