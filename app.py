b824ac899d0dbab6c582c74aac06d945a3d5c49e
Achiel van der Mandele <git@elsuxor.net>
2012-03-14 15:09:59 +0200

    map all static pages to urls for 301 redirection (when not running behind nginx). replaced all occurrences of password with secret

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import redis
-import random, string
+import random, string, os
 
-url = "http://localhost:5000"
+host = "http://localhost:5000"
 r = redis.Redis()
 app = Flask(__name__)
 
 random_char = lambda : random.choice(string.ascii_lowercase + string.digits)
 random_string = lambda x : ''.join(random_char() for x in range(x))
 
-@app.route("/index.html")
-def index():
-    return render_template('index.html')
+redirect_urls = {'/' : '/index.html'}
+for (dirpath, dirname, filenames) in os.walk('static'):
+    for f in filenames:
+        file_url = os.path.join(dirpath, f)
+        redirect_urls[file_url[6:]] = '/%s' % file_url
+
+def redirect_url():
+    return redirect(redirect_urls[request.path], 301)
+
+for url in redirect_urls:
+    app.add_url_rule(url, url, redirect_url)
 
 @app.route("/create/", methods=['POST',])
 def create():
@@ -20,7 +28,7 @@ def create():
     if not secret: 
         secret = random_string(20)
     r.set(key, secret)
-    secret_url = "%s/get/%s" % (url, key)
+    secret_url = "%s/get/%s" % (host, key)
     return render_template('created.tmpl', url=secret_url)
 
 @app.route("/generate")
@@ -28,7 +36,7 @@ def generate():
     secret = random_string(20)
     key = random_string(50)
     r.set(key, secret)
-    generated_url = "%s/get/%s" % (url, key)
+    generated_url = "%s/get/%s" % (host, key)
     return render_template('generate.tmpl', generated=secret, url=generated_url)
 
 @app.route("/get/<key>")
